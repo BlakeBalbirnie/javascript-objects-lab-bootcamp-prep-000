ab2d3bbced50df4006ae607b4a56c76cfe768d09
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-01 16:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({ obj: { prop: 1 }, key: 'prop2', value: 2 })
+  return Object.assign({}, obj, { [key]: value })
 }
