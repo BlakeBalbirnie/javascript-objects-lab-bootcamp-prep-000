438e8b76852b4749f69a15d43263519d5cb1d772
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-01 16:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({ obj, key, value })
 }
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({ obj: { prop: 1 }, key: 'prop2', value: 2 })
+}
